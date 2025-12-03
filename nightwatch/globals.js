5a17c5f2b601017f4e238e3a0285a5d70d45d4f1
Bhargavi-BS <bhargavi@browserstack.com>
2025-12-03 16:08:35 +0530

    minor change

--- nightwatch/globals.js
+++ nightwatch/globals.js
@@ -21,7 +21,7 @@ const _testCasesData = {};
 let currentTestUUID = '';
 let workerList = {};
 let testRunner = '';
-const testEventPromises = [];
+let testEventPromises = [];
 
 eventHelper.eventEmitter.on(EVENTS.LOG_INIT, (loggingData) => {
   const testCaseStartedId = loggingData.message.replace('TEST-OBSERVABILITY-PID-TESTCASE-MAPPING-', '').slice(1, -1);
